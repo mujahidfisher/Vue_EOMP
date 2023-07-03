ce92b5edae2d053dd72197dfeeb5b3ebd62fd2b0
mujahidfisher <fishermujahid@gmail.com>
2023-07-03 15:48:15 +0200

    finished contact page

---
+++ src/components/Form.vue
@@ -0,0 +1,33 @@
+<template>
+    <form
+  action="https://formspree.io/f/xwkdejzz"
+  method="POST"
+>
+<div class="form">
+    <input type="text" class="form-control" placeholder="First name" aria-label="First name">
+
+    <input type="text" class="form-control" placeholder="Last name" aria-label="Last name">
+
+    <input type="email" 
+    class="form-control" 
+    placeholder="Email:" name="email">
+
+    <textarea type="text"
+    class="form-control"
+    placeholder="Message:" name="message"></textarea>
+
+  <button class="btn btn-outline-info" type="submit">SUBMIT</button>
+</div>
+</form>
+
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
+
+<style scoped>
+
+</style>
