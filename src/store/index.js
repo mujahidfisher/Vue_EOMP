b819b1e94cb4977e2007153dba67d0b101f3f625
mujahidfisher <fishermujahid@gmail.com>
2023-07-03 16:43:56 +0200

    semi-finished

--- src/store/index.js
+++ src/store/index.js
@@ -2,12 +2,25 @@ import { createStore } from 'vuex'
 
 export default createStore({
   state: {
+    testcards: null
   },
   getters: {
   },
   mutations: {
+    setTestimonials(state, testcards) {
+      state.testcards = testcards
+    }
   },
   actions: {
+    async fetchTestimonials(context) {
+      try{
+        let response = await fetch ("https://mujahidfisher.github.io/Vue_EOMP_Json-server/data.Json")
+        let { testcards } = await response.json()
+        context.commit("setTestimonials", testcards)
+      }catch(e) {
+        console.log("nothing");
+      }
+    }
   },
   modules: {
   }
