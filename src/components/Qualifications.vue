ce1f249585cf96a2a050a33afeeac0b75a5f750e
mujahidfisher <fishermujahid@gmail.com>
2023-07-05 15:59:47 +0200

    finished educ and quali

---
+++ src/components/Qualifications.vue
@@ -0,0 +1,31 @@
+<template>
+    <div v-for="qualifications in qualifications" class="section-4" :key="qualifications.matric">
+        <h1>Qualifications</h1>
+        <p>Since Im a school leaver I do not have much behind my name but I strive to change this.</p>
+        <div class="boxy">
+            <li>
+                Qualification:{{ qualifications.matric }}
+            </li>
+            <li>
+                Currently Doning:{{ qualifications.current }}
+            </li>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        computed: {
+            qualifications() {
+                return this.$store.state.qualifications
+            }
+        },
+        mounted() {
+            this.$store.dispatch("fetchQualifications")
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
