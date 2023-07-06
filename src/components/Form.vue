8c8eb54be096d619757888625cb95f880f837027
mujahidfisher <fishermujahid@gmail.com>
2023-07-06 14:29:43 +0200

    update

--- src/components/Form.vue
+++ src/components/Form.vue
@@ -3,19 +3,23 @@
   action="https://formspree.io/f/xwkdejzz"
   method="POST"
 >
-
 <div class="form" name="form">
-    <input type="text" class="form-control" placeholder="First name" name="First name" required>
+    <input type="text" class="form-control" autocomplete="off"
+   placeholder="First name" name="First name" required oninvalid="this.setCustomValidity('Whats your Name!')"
+   oninput="setCustomValidity('')">
 
-    <input type="text" class="form-control" placeholder="Last name" name="Last name" required>
+    <input type="text" class="form-control" autocomplete="off" placeholder="Last name" name="Last name" required oninvalid="this.setCustomValidity('I need a Surname!')"
+    oninput="setCustomValidity('')">
 
     <input type="email" 
     class="form-control" 
-    placeholder="Email:" name="email" required>
+    placeholder="Email:" autocomplete="off" name="email" required oninvalid="this.setCustomValidity('Email is required as Usual!')"
+    oninput="setCustomValidity('')">
 
     <textarea type="text"
     class="form-control"
-    placeholder="Message:" name="message" required></textarea>
+    placeholder="Message:" name="message" autocomplete="off" required oninvalid="this.setCustomValidity('Dont you want to say anything!')"
+    oninput="setCustomValidity('')"></textarea>
 
   <button class="btn btn-outline-info" type="submit">SUBMIT</button>
 </div>
@@ -25,8 +29,8 @@
 
 <script>
     export default {
-        
-    }
+      
+    };
 </script>
 
 <style scoped>
