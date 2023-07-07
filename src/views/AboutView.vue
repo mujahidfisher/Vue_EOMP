7864df9fe35ec829b18eb854d41914adb3650c39
mujahidfisher <fishermujahid@gmail.com>
2023-07-07 09:43:01 +0200

    fixes

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -2,10 +2,10 @@
     <div class="aboutpage">
         <h1>About Me</h1>
         <div class="about-paragraph">
-            <p class="p1" data-aos="fade-right" data-aos-duration="1000">
+            <p class="p1" data-aos="fade-up" data-aos-duration="1000">
                 "Hi Im Mujahid. I was born in Cape Town and moved around alot. Since a kid I wanted to be those people driving fancy cars and have big houses. So my goal in life is not to be them but BETTER!"
             </p>
-            <p class="p2" data-aos="fade-left" data-aos-duration="1000">
+            <p class="p2" data-aos="fade-up" data-aos-duration="1000">
                 "I am a hard working individual if I do say so. I try my utmost best in everything I do, evens if it's a trivial task."
             </p>
             <p class="p3" data-aos="fade-up" data-aos-duration="1000">
