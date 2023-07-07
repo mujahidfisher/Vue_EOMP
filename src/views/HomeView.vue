2a0951eedefc4a97e9076c9a94a2d5f51212f4a4
mujahidfisher <fishermujahid@gmail.com>
2023-07-07 11:53:23 +0200

    router link

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -10,9 +10,9 @@
         <p>Hi my name is Mujahid.
           <br>
         Hope you find my portfolio Interesting!</p><br>
-        <a href="/resume"><button v-wave class="join bg-info ">Learn More <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-right" viewBox="0 0 16 16">
+        <router-link to="/resume"><button v-wave class="join bg-info ">Learn More <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-right" viewBox="0 0 16 16">
           <path fill-rule="evenodd" d="M1 8a.5.5 0 0 1 .5-.5h11.793l-3.147-3.146a.5.5 0 0 1 .708-.708l4 4a.5.5 0 0 1 0 .708l-4 4a.5.5 0 0 1-.708-.708L13.293 8.5H1.5A.5.5 0 0 1 1 8z"/>
-        </svg></button></a>
+        </svg></button></router-link>
       </div>
       <div class="pic">
         <img src="https://i.postimg.cc/cLjjMmyG/20230702-164359.jpg" alt="profile" class="profilepic" loading="lazy">
